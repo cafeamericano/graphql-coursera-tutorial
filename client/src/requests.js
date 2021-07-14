bf002371ff8bfe29463d22bfd9ee5184b6ee3aba
Matthew Farmer <mfarmer5102@gmail.com>
2021-07-13 20:34:55 -0400

    implemented caching and fragments

--- client/src/requests.js
+++ client/src/requests.js
@@ -23,73 +23,90 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
+const jobDetailFragment = gql`
+    fragment JobDetail on Job {
+        id
+        title
+        description
+        company {
+            id
+            name
+        }
+    }
+`;
 
-export async function loadJob(id) {
-    const query = gql`
-        query JobQuery($id: ID!) {
-            job(id: $id) {
+const jobQuery = gql`
+    query JobQuery($id: ID!) {
+        job(id: $id) { ...JobDetail }
+    }
+    ${jobDetailFragment}
+`;
+
+const jobsQuery = gql`
+    query JobsQuery {
+        jobs {
+            id
+            title
+            company {
                 id
-                title
-                description
-                company {
-                    id
-                    name
-                }
+                name
             }
         }
-    `;
-    const {data} = await client.query({query, variables: {id}});
+    }
+`;
+
+const createJobMutation = gql`
+    mutation CreateJob($input: CreateJobInput) {
+        job: createJob(input: $input) { ...JobDetail }
+    }
+    ${jobDetailFragment}
+`;
+
+const companyQuery = gql`
+    query CompanyQuery($id: ID!) {
+        company(id: $id) {
+            id
+            name
+            description
+            jobs {
+                id
+                title
+            }
+        }
+    }
+`;
+
+export async function loadJob(id) {
+    const {data} = await client.query({query: jobQuery, variables: {id}});
     return data.job;
 }
 
 export async function loadJobs() {
-    const query = gql`
-        {
-            jobs {
-                id
-                title
-                company {
-                    id
-                    name
-                }
-            }
-        }
-    `;
-    const {data} = await client.query({query});
+    const {data} = await client.query({query: jobsQuery, fetchPolicy: 'no-cache'});
+    /* 
+        Cache options
+            -- cache-first; if not in cache, make call; default
+            -- no-cache; always fetch data from server
+    */
     return data.jobs;
 }
 
 export async function loadCompany(id) {
-    const query = gql`
-        query CompanyQuery($id: ID!) {
-            company(id: $id) {
-                id
-                name
-                description
-                jobs {
-                    id
-                    title
-                }
-            }
-        }
-    `;
-    const {data} = await client.query({query, variables: {id}});
+    const {data} = await client.query({query: companyQuery, variables: {id}});
     return data.company;
 }
 
 export async function createJob(input) {
-    const mutation = gql`
-        mutation CreateJob($input: CreateJobInput) {
-            job: createJob(input: $input) {
-                id
-                title
-                company {
-                    id
-                    name
-                }
-            }
+    const {data: {job}} = await client.mutate({
+        mutation: createJobMutation, 
+        variables: {input},
+        update: (cache, {data}) => { // Called after the mutation to directly add item into the cache
+            cache.writeQuery({
+                query: jobQuery, // The query we're trying to update our cache for
+                variables: {id: data.job.id}, // Where id is the id of the job to be cached
+                data: data // Where data is the data that was returned upon insertion
+            });
         }
-    `;
-    const {data} = await client.mutate({mutation, variables: {input}});
-    return data.job;
+    });
+    return job;
 }
